0b59b5bb87eb209adedb64d355a80c8ade2eb9e3
Ahsan Khan <ahsankhancodes@gmail.com>
2024-02-26 21:09:45 +0500

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
+import 'package:flutter_catalog/screens/home_page.dart';
+import 'package:flutter_catalog/screens/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,8 +19,18 @@ class MyApp extends StatelessWidget {
     // var day = "Sunday";
     // Compiler will decide which datatype should be given to var.
 
-    return const MaterialApp(
-      home: HomePage(),
+    return MaterialApp(
+      // home: const HomePage(),
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => const HomePage(),
+        "/home": (context) => const HomePage(),
+        "/login": (context) => const LoginPage(),
+      },
     );
   }
 }
